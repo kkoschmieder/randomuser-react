d1e10c952b0baa7c0a42066d0d047eb083413356
kkoschmieder <98361397+kkoschmieder@users.noreply.github.com>
2022-06-19 19:56:06 +0200

    Release
    Changed layout and minimized date of birth data to YYYY-MM-DD.

--- src/RandomUser.jsx
+++ src/RandomUser.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 const RandomUser = ({user}) => {
+    let dateMinimized = user?.dob.date.slice(0, -14);
     return (
         <div className='userCard'>
             <img className='userPicture' src={user?.picture.large} />
             <div className='userData'>
-                <h2>Name: {user?.name.title} {user?.name.first} {user?.name.last}</h2>
-                <h2>Gender: {user?.gender}</h2>
-                <h2>Date of Birth: {user?.dob.date}</h2>
-                <h2>Age: {user?.dob.age}</h2>
-                <h2>Phone: {user?.phone}</h2>
+                <h2 className='bright'>Name: {user?.name.title} {user?.name.first} {user?.name.last}</h2>
+                <h2 className='dark'>Gender: {user?.gender}</h2>
+                <h2 className='bright'>Date of Birth: {dateMinimized}</h2>
+                <h2 className='dark'>Age: {user?.dob.age}</h2>
+                <h2 className='bright'>Phone: {user?.phone}</h2>
             </div>
         </div>
     );
